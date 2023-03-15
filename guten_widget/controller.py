235263902b0034e442aff218d1ba2d04545b25a8
Ellianna3 <bauee527@hsd.k12.or.us>
2023-03-15 15:24:30 -0700

    switched to fruityvice api

--- guten_widget/controller.py
+++ guten_widget/controller.py
@@ -4,9 +4,11 @@ format the results for display."""
 # import statements
 import requests
 
+# example url: https://fruityvice.com/api/fruit/banana
+
 #set some variables to help our API call
-domain = "https://gutendex.com/"
-endpoint = "books/?search="
+domain = "https://fruityvice.com/"
+endpoint = "api/fruit/"
 
 # Make an API call
 def make_call(query: str) -> str:
@@ -20,36 +22,23 @@ def make_call(query: str) -> str:
     response  = requests.get(url)
     results = ""
     if response.ok:
-        results = get_top_results(response)
-        results = format_results(results)
+        results = format_results(response)
     else:
         results = "There was an error. Please try later."
     return results
 
-def get_top_results(response, max=4) -> list:
-    """takes an API response and returns a list of the fist max # of results"""
-    results = response.json()["results"]
-    return results[:max]
-
 def format_results(results) -> str:
     """take results (a list), extract the info we need and return a 
     formatted string"""
-    formatted_results = ""
-    for item in results:
-        title = item.get("title")
-        formatted_results += "<p><b>Title: <i>" + title + "</i></b></p>"
-        authors = item.get("authors")
-        if len(authors) >1:
-            formatted_results += "<p>Authors</p><ul>"
-            for author_data in authors:
-                author = author_data.get("name")
-                formatted_results += "<li>" + author + "</li>"
-            formatted_results =+ "</ul>"
-        else:
-            author = authors[0].get("name")
-            formatted_results += "<p>by " + author + "</p>"
+    results = results.json()["nutritions"]
+    formatted_results = "<ul>"
+    #loop
+    for nutrition, value in results.items():
+        formatted_results += "<li><b>" + nutrition + "</b>: " + str(value) + "</li>\n"
+    formatted_results += "</ul>"
+
     return formatted_results
 
 if __name__ == "__main__":
-    call_results = make_call("douglas")
+    call_results = make_call("banana")
     print(call_results)
--- guten_widget/widget.py
+++ guten_widget/widget.py
@@ -9,8 +9,8 @@ import controller
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.resize(300, 500)
-        self.setWindowTitle("The Guten-widget")
+        self.resize(300, 300)
+        self.setWindowTitle("Fruit Facts!")
         self.setWindowIcon(QIcon("icon.jpg"))
         self.UI()
  
@@ -19,19 +19,18 @@ class Window(QWidget):
         self.setLayout(layout)
         
         #Create our Widgets
-        title_label = QLabel("Guten Search Widget")
+        title_label = QLabel("<b>Fruit Nutrition Widget</b>")
         title_label.setAlignment(Qt.AlignmentFlag.AlignTop)
-        title_label.setFont(QFont("Calibri", 22))
+        title_label.setFont(QFont("Calibri", 25))
 
-        description = "Search the Gutenberg Project by title,"
-        description += "author, or subject."
+        description = "Search Fruityvice API for Fruit Nutrition Facts"
         description_label = QLabel(description)
-        description_label.setFont(QFont("Calibri", 14))
+        description_label.setFont(QFont("Calibri", 15))
 
         search_layout = QHBoxLayout()
         self.search_field = QLineEdit()
-        self.search_field.setFont(QFont("Calibri", 12))
-        self.search_field.setPlaceholderText("title, author, subject")
+        self.search_field.setFont(QFont("Calibri", 14))
+        self.search_field.setPlaceholderText("Enter a fruit name here")
 
         search_button = QPushButton("Search")
         search_button.setFont(QFont("Calibri", 12))
@@ -41,7 +40,7 @@ class Window(QWidget):
         search_layout.addWidget(search_button)
 
         self.results_text = QTextEdit("<h2>Results.</h2>")
-        self.results_text.setFont(QFont("Calibri", 12))
+        self.results_text.setFont(QFont("Calibri", 13))
 
         # Add all our widgets
         layout.addWidget(title_label)
@@ -70,7 +69,7 @@ def main():
     # Set styles
     app.setStyleSheet("""
         QWidget {
-            background-color: #336699;
+            background-color: #540000;
             color: #f1f1f1;
         }
         h2{
